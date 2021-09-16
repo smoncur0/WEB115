b9feb1430b69af064301afb97501bc583272bc9a
smoncur0 <78877161+smoncur0@users.noreply.github.com>
2021-09-16 16:08:36 -0400

    new

--- fizzBuzz/fizzBuzz_java.js
+++ fizzBuzz/fizzBuzz_java.js
@@ -8,18 +8,28 @@ function formSubmit() {
   let lastName = document.getElementById('last_name').value;
   let middleInitial = document.getElementById('middle_initial').value;
         
-   document.getElementById("greeting").textContent = "Welcome to Syd's Recipies, " + firstName + " " + middleInitial + ". " + lastName + "!";
-  buzz();
+document.getElementById("greeting").textContent = "Welcome to Syd's Recipies, " + firstName + " " + middleInitial + ". " + lastName + "!";
+let count = prompt(`How high do you want to count, ${firstName}?`);
+buzz(count);
 }
 
-function buzz() {
-  for(let i = 0; i < 125; i++) {
+function buzz(counter) {
+for(let i = 0; i <= counter; i++) {
+  var evenOdd;
+  
+  if (i % 2 === 0) {
+    evenOdd = 'this is an even number';
+  } else {
+    evenOdd = 'this is an odd number';
+  }
+
+
 
     //creates new element & stores in variable
     var element_1 = document.createElement('li');
     
     //text node variable and components
-    var text = document.createTextNode(i + ' Easy Peasy');
+    var text = document.createTextNode(i + ' Easy Peasy ||' + evenOdd);
     
     //text node and new element attach
     element_1.appendChild(text);
