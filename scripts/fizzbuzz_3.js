37b2dc119274af7af0aed53abb8d55a142cd0430
smoncur0 <78877161+smoncur0@users.noreply.github.com>
2021-10-29 16:26:52 -0400

    Update fizzbuzz_3.js

--- scripts/fizzbuzz_3.js
+++ scripts/fizzbuzz_3.js
@@ -34,13 +34,13 @@ function solve(firstvalue, secondvalue, thirdvalue)
   
       let fizzSolve;
       
-      if (i, value1(solve)) {
+      if ((i, value1)solve {
           fizzSolve = "Fun";
       
-      } else if(i, value2(solve)) { 
+      } else if((i, value2)solve) { 
           fizzSolve = "Quick";
 
-      } else (i, value3(solve))  
+      } else ((i, value3)solve)  
       {
           fizzSolve = "BANG!";
       }
