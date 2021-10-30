23878b932deea30a4a7a2e234f3e2773fd30f220
smoncur0 <78877161+smoncur0@users.noreply.github.com>
2021-10-30 19:53:43 -0400

    Update fizzbuzz_3.js

--- scripts/fizzbuzz_3.js
+++ scripts/fizzbuzz_3.js
@@ -26,8 +26,8 @@ function checkDivision(div1, div2) {
           return false;
   }   
 }
-function buzz(div1, div2, div3, limit) {
-  for(let i = 1; i <= limit; i++) {
+function buzz(div1, div2, div3, count) {
+  for(let i = 1; i <= count; i++) {
 
     //creates new element & stores in variable for loop
     let fizzDivision;
