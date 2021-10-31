871ffb10f8213b42283dd6bf0919c00c09986000
smoncur0 <78877161+smoncur0@users.noreply.github.com>
2021-10-30 21:40:18 -0400

    Update fizzbuzz_2.js

--- scripts/fizzbuzz_2.js
+++ scripts/fizzbuzz_2.js
@@ -2,13 +2,13 @@
 //sydney m
 
 
-
+//uses information entered by user to make the form function
 function formSubmit() {
-    let first = document.getElementById('first').value;
-    let last = document.getElementById('last').value;
-    let middle = document.getElementById('middle').value;  
- 
-     document.getElementById("greeting").textContent = "Welcome to Syd's Shoe Styles, " + first + " " + middle + ". " + last + "!";
+  let first_Name = document.getElementById('first_name').value;
+  let last_Name = document.getElementById('last_name').value;
+  let middle_initial = document.getElementById('middle_initial').value;  
+
+   document.getElementById("greeting").textContent = "Welcome to Syd's Shoe Styles, " + first_Name + " " + middle_initial + ". " + last_Name + "!";
      let div1 = 3;
      let div2 = 5;
      let count = 140;
@@ -24,12 +24,13 @@ function formSubmit() {
             return false;
     }   
   }
+
   function buzz(div1, div2, countLimit) {
     for(let i = 1; i <= countLimit; i++) {
   
-      //creates new element & stores in variable for loop
-      var fizzDivision;
-      
+   
+      let fizzDivision;
+   //conditions for loop to run   
       if (i % div1 === 0) {
           fizzDivision = "Fun";
       
@@ -41,19 +42,18 @@ function formSubmit() {
       }
       
     
-    //creates new element & stores in variable
+ //helps the page run within a list
     var element_1 = document.createElement('li');
-    
-    //text node variable and components
+  
     var text = document.createTextNode(i + ' ' + fizzDivision);
     
-    //text node and new element attach
+ 
     element_1.appendChild(text);
     
-    //element's postition
+  
     var pos_new = document.getElementsByTagName('ul')[0];
     
-    //changes position
+
     pos_new.appendChild(element_1);
     
   }
