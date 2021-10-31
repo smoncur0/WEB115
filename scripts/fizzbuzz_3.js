6f74e24685c79b245bd475e09b38cb452641acc0
smoncur0 <78877161+smoncur0@users.noreply.github.com>
2021-10-30 20:10:32 -0400

    Update fizzbuzz_3.js

--- scripts/fizzbuzz_3.js
+++ scripts/fizzbuzz_3.js
@@ -6,9 +6,9 @@
 function formSubmit() {
   let first_Name = document.getElementById('first_name').value;
   let last_Name = document.getElementById('last_name').value;
-  let middle_Initial = document.getElementById('middle_initial').value;  
+  let middle_initial = document.getElementById('middle_initial').value;  
 
-   document.getElementById("greeting").textContent = "Welcome to Syd's Shoe Styles, " + first_Name + " " + middle_Initial + ". " + last_Name + "!";
+   document.getElementById("greeting").textContent = "Welcome to Syd's Shoe Styles, " + first_Name + " " + middle_initial + ". " + last_Name + "!";
    let div1 = 3;
    let div2 = 5;
    let div3 = 7;
