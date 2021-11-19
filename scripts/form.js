987c4180b68bfb9f1d9bdcee54355065898eb605
smoncur0 <78877161+smoncur0@users.noreply.github.com>
2021-11-19 10:49:09 -0500

    Update form.js

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,13 @@
 function formData() {
-    var firstName = document.getElementById('first_name').value;
-    var lastName = document.getElementById('last_name').value;
+    var first_name = document.getElementById('first_name').value;
+    var last_name = document.getElementById('last_name').value;
     var personal_back = "Personal Background: " + document.getElementById('personal_back').value;
     var academic_back = "Academic Background: " + document.getElementById('academic_back').value;
     var computer_platform = "Primary Computer Platform: " + document.getElementById('computer_platform').value;
     var course_list = "Courses I'm Taking, and Why: " + document.getElementById('course_list').value;
     var funny_item = "Funny/Interesting item about yourself: " + document.getElementById('funny_item').value;
     document.getElementById("greeting").textContent = "Welcome to Syd's Shoe Styles, " + first_name + " " + last_name + "!";
+    
     
     introData(personal_back, academic_back, computer_platform, course_list, funny_item);
   
