510f9a1b6d641aff1af7499837d8a74cb821eb85
smoncur0 <78877161+smoncur0@users.noreply.github.com>
2021-11-19 13:39:41 -0500

    Update form.js

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,12 @@
 function formData() {
-    var first_name = document.getElementById('first_name').value;
-    var last_name = document.getElementById('last_name').value;
-    var personal_back = "Personal Background: " + document.getElementById('personal_back').value;
-    var academic_back = "Academic Background: " + document.getElementById('academic_back').value;
-    var computer_platform = "Primary Computer Platform: " + document.getElementById('computer_platform').value;
-    var course_list = "Courses I'm Taking, and Why: " + document.getElementById('course_list').value;
-    var funny_item = "Funny/Interesting item about yourself: " + document.getElementById('funny_item').value;
     document.getElementById("greeting").textContent = "Welcome to Syd's Shoe Styles, " + first_name + " " + last_name + "!";
+    let first_name = document.getElementById('first_name').value;
+    let last_name = document.getElementById('last_name').value;
+    let personal_back = "Personal Background: " + document.getElementById('personal_back').value;
+    let academic_back = "Academic Background: " + document.getElementById('academic_back').value;
+    let computer_platform = "Primary Computer Platform: " + document.getElementById('computer_platform').value;
+    let course_list = "Courses I'm Taking, and Why: " + document.getElementById('course_list').value;
+    let funny_item = "Funny/Interesting item about yourself: " + document.getElementById('funny_item').value;
     
     
     introData = [personal_back, academic_back, computer_platform, course_list, funny_item];
@@ -15,13 +15,13 @@ function formData() {
   }
   
   
-  function display(introData) {
+  function display(inputData) {
   
     for (let i = 0; i < introData.length; i++) {
-        var element_1 = document.createElement('li');
-        var text = document.createTextNode(i + ' ' + introData);
+        let element_1 = document.createElement('li');
+        let text = document.createTextNode(i + ' ' + introData);
         element_1.appendChild(text);
-        var pos_new = document.getElementsByTagName('ul')[0];
+        let pos_new = document.getElementsByTagName('ul')[0];
         pos_new.appendChild(element_1);
     }
   
